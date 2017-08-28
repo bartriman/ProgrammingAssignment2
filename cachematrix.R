4456f4623d10508d1dd0363c6210d10dca2c6d62
bartriman <bartek.bielski@gmail.com>
2017-08-28 16:47:28 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
+## To save time and computing power by using invertex matrices, the following functions 
+## (a) create cached variable that contain value of inversed matrix and
+## (b) check if particular matrix was inversed and cached and if yes, the returns it. If not, then inversion is calculated.
 ## functions do
 
-## Write a short comment describing this function
+## The following function creates special variable (list containing another function to store
+##    (a) value of the matrix and 
+##    (b) cached value of inverted matrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ci <- NULL 
+  set <- function(y) { 
+    x <<- y
+    ci <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) ci <<- solve
+  getInv <- function() ci
+  list(set = set, 
+       get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## The function below checks if particular matrix was inversed and cached and if yes, returns it. 
+##  If not, then inversion is calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ci <- x$getInv()
+  if(!is.null(ci)) {
+    message("..getting cached data..")
+    return(ci)
+  }
+  message("..calculating data..")
+  data <- x$get()
+  ci <- solve(data, ...)
+  x$setInv(ci)
+  ci
+  
+    ## Return a matrix that is the inverse of 'x'
 }
